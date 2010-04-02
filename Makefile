c60115e7fa1f0e9fb14a77217cb2d3156377aa5a
Linus Akesson <linus@linusakesson.net>
2010-04-02 23:49:15 +0200

    OpenGL

--- Makefile
+++ Makefile
@@ -5,11 +5,11 @@ all: klamrisk
 both: klamrisk klamrisk-win32.exe
 
 klamrisk-win32.exe: klamrisk.c
-	i686-mingw32-gcc $^ -lmingw32 -lSDLmain -lSDL -o $@ 
+	i686-mingw32-gcc $^ -lmingw32 -lSDLmain -lSDL -lGL -lGLU -o $@ 
 	i686-mingw32-strip $@
 
 klamrisk: klamrisk.c
-	$(CC) -o $@ $^ -lSDL
+	$(CC) -o $@ $^ -lSDL -lGL -lGLU
 	strip $@
 
 clean:
--- klamrisk.c
+++ klamrisk.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <math.h>
 #include <SDL/SDL.h>
+#include <SDL/SDL_opengl.h>
 
+int screen_width, screen_height;
+
+// Legacy, not related to actual dimensions:
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
-#define COLORKEY 255, 0, 255 //Your Transparent colour
 
-#define KLAMRISK_BG 0xfdff32
-#define WHITE 0xffffff
-#define GREY 0x777777
-#define BLACK 0x0
-
-#define LEFTSIDE 290
-#define RIGHTSIDE 350
+#define WALLPOS 30
+#define SPLATTERPOS 20
 
 #define CIRCLEMAX 32
+#define CIRCLEDIM 128
 #define NPARTICLE 256
 
 #define DOORHEIGHT 80
@@ -22,15 +22,8 @@
  
 SDL_Surface *screen;
 
-// White elevator shaft
-SDL_Rect outershaft = { 238, 0, 164, SCREEN_HEIGHT };
-SDL_Rect shaft = { 240, 0, 160, SCREEN_HEIGHT };
-SDL_Rect leftwall = {LEFTSIDE, 0, 1, SCREEN_HEIGHT};
-SDL_Rect rightwall = {RIGHTSIDE, 0, 1, SCREEN_HEIGHT};
-
 enum dir { LEFT, RIGHT };
 
-char circlebuf[CIRCLEMAX][CIRCLEMAX];
 struct particle {
 	int	x, y;
 	int	dx, dy;
@@ -38,32 +31,54 @@ struct particle {
 	int	r;
 } particle[NPARTICLE];
  
+enum {
+	T_CIRCLE,
+};
+
 static int init_video(Uint32 flags) {
+	SDL_Rect **modes;
+
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
 		fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
 		return 0;
 	}
 	atexit(SDL_Quit); 
 
-	screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 24, flags);
+	modes = SDL_ListModes(0, SDL_FULLSCREEN | SDL_HWSURFACE | SDL_OPENGL);
+	if(modes == 0 || modes == (SDL_Rect **) -1) {
+		screen_width = 640;
+		screen_height = 480;
+	} else {
+		screen_width = modes[0]->w;
+		screen_height = modes[0]->h;
+	}
+
+	screen = SDL_SetVideoMode(screen_width, screen_height, 0, flags | SDL_HWSURFACE | SDL_OPENGL);
 	if (!screen) {
 		fprintf(stderr, "Unable to set video mode: %s\n", SDL_GetError());
 		return 0;
 	}
+
+	SDL_ShowCursor(0);
+
 	return 1;
 }
 
 static void precalc() {
 	int x, y;
+	uint8_t circlebuf[CIRCLEDIM][CIRCLEDIM];
 
-	for(y = 0; y < CIRCLEMAX; y++) {
-		for(x = 0; x < CIRCLEMAX; x++) {
+	for(y = 0; y < CIRCLEDIM; y++) {
+		for(x = 0; x < CIRCLEDIM; x++) {
 			int r = floor(sqrt(
-				(y - CIRCLEMAX/2) * (y - CIRCLEMAX/2) +
-				(x - CIRCLEMAX/2) * (x - CIRCLEMAX/2)));
-			circlebuf[y][x] = r;
+				(y - CIRCLEDIM/2) * (y - CIRCLEDIM/2) +
+				(x - CIRCLEDIM/2) * (x - CIRCLEDIM/2)));
+			circlebuf[y][x] = (r < CIRCLEDIM/2)? 0xff : 0x00;
 		}
 	}
+
+	glBindTexture(GL_TEXTURE_2D, T_CIRCLE);
+	gluBuild2DMipmaps(GL_TEXTURE_2D, GL_ALPHA, CIRCLEDIM, CIRCLEDIM, GL_ALPHA, GL_UNSIGNED_BYTE, circlebuf);
 }
 
 static void splatter(int x, int y) {
@@ -79,58 +94,68 @@ static void splatter(int x, int y) {
 	}
 }
 
-static void draw_door(int y, enum dir side)
-{
-	SDL_Rect floor = { side == LEFT ? LEFTSIDE - 50 : RIGHTSIDE, y, 50, 1};
-	SDL_Rect roof = { side == LEFT ? LEFTSIDE - 50 : RIGHTSIDE, y - DOORHEIGHT, side==LEFT ? 51:50, 1};
-	SDL_Rect wall = { side == LEFT ? LEFTSIDE : RIGHTSIDE, y - DOORHEIGHT, 1, DOORHEIGHT};
-	SDL_Rect door = { side == LEFT ? LEFTSIDE - 8: RIGHTSIDE + 3, y - DOORHEIGHT, 5, DOORHEIGHT};
-	SDL_Rect doorinside = { side == LEFT ? LEFTSIDE - 7: RIGHTSIDE + 4, y - DOORHEIGHT +2, 3, DOORHEIGHT-3};
+static void fillrect(double x1, double y1, double x2, double y2) {
+	glDisable(GL_TEXTURE_2D);
+	glBegin(GL_QUADS);
+	glVertex2d(x1, y1);
+	glVertex2d(x1, y2);
+	glVertex2d(x2, y2);
+	glVertex2d(x2, y1);
+	glEnd();
+}
 
-	// Clear wall
-	SDL_FillRect(screen, &wall, WHITE);
-
-	SDL_FillRect(screen, &floor, BLACK);
-	SDL_FillRect(screen, &roof, BLACK);
-	SDL_FillRect(screen, &door, BLACK);
-	SDL_FillRect(screen, &doorinside, WHITE);
+static void draw_door(int y, enum dir side) {
+	if(side == RIGHT) {
+		glScaled(-1, 1, 1);
+	}
+	glColor3d(1, 1, 1);
+	fillrect(-WALLPOS-2, y - DOORHEIGHT, -WALLPOS+1, y);
+	glColor3d(0, 0, 0);
+	fillrect(-WALLPOS-50, y, -WALLPOS-1, y + 2);
+	fillrect(-WALLPOS-50, y - DOORHEIGHT - 2, -WALLPOS-1, y - DOORHEIGHT);
+	fillrect(-WALLPOS-10, y - DOORHEIGHT, -WALLPOS-8, y);
+	fillrect(-WALLPOS-6, y - DOORHEIGHT, -WALLPOS-4, y);
+	if(side == RIGHT) {
+		glScaled(-1, 1, 1);
+	}
 }
 
 static void draw_circle(int xpos, int ypos, int r) {
 	int x, y;
 
-	for(y = -r; y < r; y++) {
-		if(ypos + y >= 0 && ypos + y < SCREEN_HEIGHT) {
-			for(x = -r; x < r; x++) {
-				if(xpos + x >= LEFTSIDE && xpos + x < RIGHTSIDE) {
-					if(circlebuf[y + CIRCLEMAX/2][x + CIRCLEMAX/2] < r) {
-						uint8_t *pix = (uint8_t *) screen->pixels + screen->pitch * (ypos + y) + 3 * (xpos + x);
-						pix[0] = 0x00;
-						pix[1] = 0x00;
-						pix[2] = 0xff;
-					}
-				}
-			}
-		}
-	}
+	glColor3d(1, 0, 0);
+	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glBegin(GL_QUADS);
+	glTexCoord2d(0, 0);
+	glVertex2d(xpos - r, ypos - r);
+	glTexCoord2d(0, 1);
+	glVertex2d(xpos - r, ypos + r);
+	glTexCoord2d(1, 1);
+	glVertex2d(xpos + r, ypos + r);
+	glTexCoord2d(1, 0);
+	glVertex2d(xpos + r, ypos - r);
+	glEnd();
+	glDisable(GL_BLEND);
+	glDisable(GL_TEXTURE_2D);
 }
 
 static void draw_lift(enum dir side)
 {
-	SDL_Rect floor = { LEFTSIDE + 1, FLOOR, 60, 3};
-	SDL_Rect floorinside = { LEFTSIDE + 2, FLOOR+1, 57, 1};
-	SDL_Rect tunna = { side == LEFT ? LEFTSIDE : RIGHTSIDE - 25, FLOOR-40, 25, 40};
+	glColor3d(0, 0, 0);
+	fillrect(-WALLPOS, FLOOR, WALLPOS, FLOOR + 2);
+	fillrect(-WALLPOS, FLOOR - DOORHEIGHT, WALLPOS, FLOOR - DOORHEIGHT + 2);
 
-	SDL_FillRect(screen, &floor, BLACK);
-	SDL_FillRect(screen, &floorinside, WHITE);
-	SDL_FillRect(screen, &tunna, BLACK);
+	//SDL_Rect tunna = { side == LEFT ? LEFTSIDE : RIGHTSIDE - 25, FLOOR-40, 25, 40};
+	//SDL_FillRect(screen, &tunna, BLACK);
 }
 
 int main(int argc, char *argv[])
 {
 	int i;
 
-	init_video(SDL_DOUBLEBUF); //|SDL_FULLSCREEN);
+	init_video(0); // SDL_FULLSCREEN);
 	precalc();
 
 	int running = 1;
@@ -153,7 +178,7 @@ int main(int argc, char *argv[])
 					if (y >= FLOOR && y <= FLOOR +3 && side == door) {
 						// DIIEEEEEEE!!!
 						alive = 0;
-						splatter(side == LEFT ? RIGHTSIDE - 10 : LEFTSIDE + 10, FLOOR - 45);
+						splatter(side == LEFT ? SPLATTERPOS : -SPLATTERPOS, FLOOR - 45);
 					}
 				} else {
 					// Start new door
@@ -164,7 +189,7 @@ int main(int argc, char *argv[])
 			for(i = 0; i < NPARTICLE; i++) {
 				if(particle[i].r > 0 && particle[i].y < (SCREEN_HEIGHT + CIRCLEMAX) * 8) {
 					particle[i].x += particle[i].dx;
-					if(particle[i].x > RIGHTSIDE * 8) {
+					if(particle[i].x > WALLPOS * 8) {
 						particle[i].x -= particle[i].dx;
 						particle[i].dx *= -.1;
 						if(rand() % 3) {
@@ -173,7 +198,7 @@ int main(int argc, char *argv[])
 							particle[i].dx = 0;
 						}
 					}
-					if(particle[i].x < LEFTSIDE * 8) {
+					if(particle[i].x < -WALLPOS * 8) {
 						particle[i].x -= particle[i].dx;
 						particle[i].dx *= -.1;
 						if(rand() % 3) {
@@ -213,7 +238,7 @@ int main(int argc, char *argv[])
 							}
 							break;
 						case SDLK_x:
-							splatter(SCREEN_WIDTH / 2 - 10, SCREEN_HEIGHT / 2);
+							splatter(-10, SCREEN_HEIGHT / 2);
 							break;
 						case SDLK_ESCAPE:
 							running = 0;
@@ -224,14 +249,28 @@ int main(int argc, char *argv[])
 		}
 
 		// Set background
-		SDL_FillRect(screen, NULL, KLAMRISK_BG);
+		glClearColor(.992, 1, .196, 0);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-		SDL_FillRect(screen, &outershaft, GREY);
-		SDL_FillRect(screen, &shaft, WHITE);
-		SDL_FillRect(screen, &leftwall, BLACK);
-		SDL_FillRect(screen, &rightwall, BLACK);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(
+			-SCREEN_WIDTH/2 * ((double) screen_width / screen_height) / ((double) SCREEN_WIDTH / SCREEN_HEIGHT),
+			SCREEN_WIDTH/2 * ((double) screen_width / screen_height) / ((double) SCREEN_WIDTH / SCREEN_HEIGHT),
+			SCREEN_HEIGHT,
+			0,
+			-1,
+			1);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
 
-		draw_door(y, door);
+		glColor3d(.47, .47, .47);
+		fillrect(-82, 0, 82, 480);
+		glColor3d(1, 1, 1);
+		fillrect(-80, 0, 80, 480);
+		glColor3d(0, 0, 0);
+		fillrect(-WALLPOS-2, 0, -WALLPOS, 480);
+		fillrect(WALLPOS, 0, WALLPOS+2, 480);
 
 		for(i = 0; i < NPARTICLE; i++) {
 			if(particle[i].r > 0 && particle[i].y < (SCREEN_HEIGHT + CIRCLEMAX) * 8) {
@@ -239,10 +278,15 @@ int main(int argc, char *argv[])
 			}
 		}
 
+		glColor3d(1, 1, 1);
+		fillrect(-80, 0, -WALLPOS-2, 480);
+		fillrect(WALLPOS+2, 0, 80, 480);
+
+		draw_door(y, door);
 		draw_lift(side);
 
 		// Flip
-		SDL_Flip(screen);
+		SDL_GL_SwapBuffers();
 	}
 	return 0;
 }
