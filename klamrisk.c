0ed913c6ae3bd1af35927efc7ec78e46a3723911
Erik Ekman <erik@kryo.se>
2010-04-03 15:33:47 +0200

    try render text

--- klamrisk.c
+++ klamrisk.c
@@ -129,15 +129,83 @@ static void load_font() {
 #ifdef WIN32
 	int len = (int) &binary_Allerta_allerta_medium_ttf_end;
 	len -= (int) &binary_Allerta_allerta_medium_ttf_start;
-	SDL_RWops *font = SDL_RWFromMem(&binary_Allerta_allerta_medium_ttf_start, len);
+	SDL_RWops *fontdata = SDL_RWFromMem(&binary_Allerta_allerta_medium_ttf_start, len);
 #else
 	int len = (int) &_binary_Allerta_allerta_medium_ttf_end;
 	len -= (int) &_binary_Allerta_allerta_medium_ttf_start;
-	SDL_RWops *font = SDL_RWFromMem(&_binary_Allerta_allerta_medium_ttf_start, len);
+	SDL_RWops *fontdata = SDL_RWFromMem(&_binary_Allerta_allerta_medium_ttf_start, len);
 #endif
 
-	SDL_FreeRW(font);
+	font =TTF_OpenFontRW(fontdata, 1, 52);
 }
+
+static int nextpoweroftwo(int x) {
+	double logbase2 = log(x) / log(2);
+	return round(pow(2,ceil(logbase2)));
+}
+
+static void render_text(char *text, TTF_Font *font, SDL_Color color, SDL_Rect *location) {
+	SDL_Surface *initial;
+	SDL_Surface *intermediary;
+	SDL_Rect rect;
+	int w,h;
+	int texture;
+
+	/* Use SDL_TTF to render our text */
+	initial = TTF_RenderText_Blended(font, text, color);
+
+	/* Convert the rendered text to a known format */
+	w = nextpoweroftwo(initial->w);
+	h = nextpoweroftwo(initial->h);
+
+	intermediary = SDL_CreateRGBSurface(0, w, h, 32, 
+			0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000);
+
+	SDL_BlitSurface(initial, 0, intermediary, 0);
+
+	/* Tell GL about our new texture */
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, 4, w, h, 0, GL_BGRA, 
+			GL_UNSIGNED_BYTE, intermediary->pixels );
+
+	/* GL_NEAREST looks horrible, if scaled... */
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);	
+
+	/* prepare to render our texture */
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	/* Draw a quad at location */
+	glBegin(GL_QUADS);
+	/* Recall that the origin is in the lower-left corner
+	 * 		   That is why the TexCoords specify different corners
+	 * 		   		   than the Vertex coors seem to. */
+	glTexCoord2f(0.0f, 1.0f); 
+	glVertex2f(location->x    , location->y);
+	glTexCoord2f(1.0f, 1.0f); 
+	glVertex2f(location->x + w, location->y);
+	glTexCoord2f(1.0f, 0.0f); 
+	glVertex2f(location->x + w, location->y + h);
+	glTexCoord2f(0.0f, 0.0f); 
+	glVertex2f(location->x    , location->y + h);
+	glEnd();
+
+	/* Bad things happen if we delete the texture before it finishes */
+	glFinish();
+
+	/* return the deltas in the unused w,h part of the rect */
+	location->w = initial->w;
+	location->h = initial->h;
+
+	/* Clean up */
+	SDL_FreeSurface(initial);
+	SDL_FreeSurface(intermediary);
+	glDeleteTextures(1, &texture);
+}
+
 
 // *************** Graphic primitives *************** 
 
@@ -262,7 +330,10 @@ static void draw_shaft(struct shaft *shaft, struct doors *left, struct doors *ri
 }
 
 static void drawtitle() {
-	;
+	SDL_Rect pos = { 0.5, 0, 0.3, 0.5 };
+	SDL_Color blk = {0,0,0};
+
+	render_text("VARNING KLÄMRISK!", font, blk, &pos);
 }
 
 static void drawframe() {
